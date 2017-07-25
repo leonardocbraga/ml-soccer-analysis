65518f7e098ca3decdeb21e7268b457265a6edf4
Leonardo Braga <leonardokcardozo@gmail.com>
2017-07-25 13:36:12 -0300

    Extracting player ratings for each soccer match

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -4,43 +4,50 @@ import os
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
+import os.path
 
 DATA_PATH = '../data/'
 DB_NAME = 'database.sqlite'
 DB_NAME_ZIP = 'database.sqlite.zip'
 
-zip_ref = zipfile.ZipFile(DATA_PATH + DB_NAME_ZIP, 'r')
-zip_ref.extractall(DATA_PATH)
-zip_ref.close()
+file_name = DATA_PATH + DB_NAME
+if not os.path.isfile(file_name):
+    zip_ref = zipfile.ZipFile(DATA_PATH + DB_NAME_ZIP, 'r')
+    zip_ref.extractall(DATA_PATH)
+    zip_ref.close()
 
-conn = sqlite3.connect(DATA_PATH + DB_NAME)
+conn = sqlite3.connect(file_name)
 
 player_df = pd.read_sql_query("SELECT * FROM Player;", conn)
-
+match_df = pd.read_sql_query("SELECT * FROM Match;", conn)
 player_att_df = pd.read_sql_query("SELECT * FROM Player_Attributes;", conn)
-player_df['rating'] = player_df['player_api_id'].apply(lambda player_id: player_att_df[player_att_df['player_api_id'] == player_id]['overall_rating'].mean())
 
+player_df['rating'] = player_df['player_api_id'].apply(lambda player_id: player_att_df[player_att_df['player_api_id'] == player_id]['overall_rating'].mean())
+ratings_dict = dict([(id, rating) for id, rating in zip(player_df.player_api_id, player_df.rating)])
+
+home_player_cols = ['home_player_' + str(i) for i in xrange(1, 12)]
+away_player_cols = ['away_player_' + str(i) for i in xrange(1, 12)]
+player_cols = home_player_cols + away_player_cols
+
+home_rating_cols = ['home_rating_' + str(i) for i in xrange(1, 12)]
+away_rating_cols = ['away_rating_' + str(i) for i in xrange(1, 12)]
+rating_cols = home_rating_cols + away_rating_cols
+
+match_df = match_df.dropna(subset=player_cols)
+match_df[rating_cols] = match_df[player_cols].apply(lambda row: row.apply(lambda pid: ratings_dict[pid]))
+
+# wins
 team_df = pd.read_sql_query("SELECT * FROM Team;", conn)
 
 team_att_df = pd.read_sql_query("SELECT * FROM Team_Attributes;", conn)
 
-match_df = pd.read_sql_query("SELECT * FROM Match;", conn)
+
 match_df['home_win'] = match_df.apply(lambda row: 1 if row['home_team_goal'] > row['away_team_goal'] else 0, axis = 1)
 match_df['away_win'] = match_df.apply(lambda row: 1 if row['home_team_goal'] < row['away_team_goal'] else 0, axis = 1)
 home_winners = match_df.groupby('home_team_api_id')['home_win'].sum()
 away_winners = match_df.groupby('away_team_api_id')['away_win'].sum()
 
 team_att_df['n_win'] = team_att_df['team_api_id'].apply(lambda team_id: home_winners[team_id] + away_winners[team_id])
-
-home_player_cols = []
-away_player_cols = []
-for i in xrange(1, 12):
-    home_player_cols.append('home_player_' + str(i))
-    away_player_cols.append('away_player_' + str(i))
-
-match_df = match_df.dropna(subset=home_player_cols)
-match_df[home_player_cols].apply(lambda row: player_df[player_df['player_api_id'] == row]['rating'])
-match_df[home_player_cols].mean(axis = 1)
 
 
 # test
